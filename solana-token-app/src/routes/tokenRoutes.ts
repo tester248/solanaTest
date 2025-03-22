f1cea01c78394506fe848410e07c8dd1e984fadc
tester248 <amathur0821@gmail.com>
2025-03-22 12:22:06 +0000

    equinox update

--- solana-token-app/src/routes/tokenRoutes.ts
+++ solana-token-app/src/routes/tokenRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { transferToken } from '../controllers/tokenController';
+import { transferEquinoxToken } from '../transferEquinoxToken'
 
 const router = Router();
 
 router.post('/transfer', transferToken);
+router.post('/equinoxtransfer', transferEquinoxToken);
 
 export default router;
